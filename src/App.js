fb695fa6dfcee398f3cd90341a5d9b59c7ae857e
MiltonTomasino <jonathantomasino13@gmail.com>
2025-01-13 16:40:59 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import ApiTest from './Components/api-test';
+import TopSongs from './Components/top-songs';
+import Search from './Components/search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spotify API Test</h1>
       </header>
+      <TopSongs />
+      <Search />
+      <p>Website made by Milton Tomasino<br></br>Contact: jonathantomasino13@gmail.com</p>
     </div>
   );
 }
